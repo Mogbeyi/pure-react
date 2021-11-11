c76f4f6c84eb2a6e60101c80663b7662f3349d47
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2021-11-11 07:09:34 +0100

    Format code

--- github-file-list/src/index.js
+++ github-file-list/src/index.js
@@ -1,55 +1,94 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+import "./index.css";
 
-const FileList = ({files}) => (
-    <table className="file-list">
-        <tbody>
-            {files.map(file => (
-                <FileListItem file={file} key={file.id} />
-            ))}
-        </tbody>
-    </table>
-)
+const FileList = ({ files }) => (
+  <table className="file-list">
+    <tbody>
+      {files.map((file) => (
+        <FileListItem file={file} key={file.id} />
+      ))}
+    </tbody>
+  </table>
+);
 
 FileList.propTypes = {
-    files: PropTypes.array
-}
+  files: PropTypes.array,
+};
 
-const FileListItem = ({file}) => (
-    <tr className="file-list-item>">
-        <td className="file-name">{file.name}</td>
-    </tr>
-)
+const FileListItem = ({ file }) => (
+  <tr className="file-list-item>">
+    <td className="file-name">{file.name}</td>
+  </tr>
+);
 
 FileListItem.propTypes = {
-    file: PropTypes.object.isRequired
+  file: PropTypes.object.isRequired,
+};
+
+function FileIcon({ file }) {
+  let icon = "fa-file-text-o";
+
+  if (file.type === "folder") {
+    icon = "fa-folder";
+  }
+
+  return (
+    <td className="file-icon">
+      <i className={`fa ${icon}`} />
+    </td>
+  );
 }
 
+FileIcon.propTypes = {
+  file: PropTypes.object.isRequired,
+};
 
-const testFiles = [ {
+function FileName({ file }) {
+  return (
+    <>
+      <FileIcon file={file} />
+      <td className="file-name">{file.name}</td>
+    </>
+  );
+}
+
+FileName.propTypes = {
+  file: PropTypes.object.isRequired,
+};
+
+const testFiles = [
+  {
     id: 1,
-    name: 'src',
-    type: 'folder',
-    updated_at: "2016-07-11 21:24:00", latestCommit: {
-    message: 'Initial commit' }
-    }, {
+    name: "src",
+    type: "folder",
+    updated_at: "2016-07-11 21:24:00",
+    latestCommit: {
+      message: "Initial commit",
+    },
+  },
+  {
     id: 2,
-    name: 'tests',
-    type: 'folder',
-    updated_at: "2016-07-11 21:24:00", latestCommit: {
-    message: 'Initial commit' }
-    }, {
+    name: "tests",
+    type: "folder",
+    updated_at: "2016-07-11 21:24:00",
+    latestCommit: {
+      message: "Initial commit",
+    },
+  },
+  {
     id: 3,
-    name: 'README',
-    type: 'file',
-    updated_at: "2016-07-18 21:24:00", latestCommit: {
-    message: 'Added a readme' }
-    }, ];
-    
+    name: "README",
+    type: "file",
+    updated_at: "2016-07-18 21:24:00",
+    latestCommit: {
+      message: "Added a readme",
+    },
+  },
+];
 
 ReactDOM.render(
-    <FileList files={testFiles}/>,
-    document.querySelector("#root")
-)
+  <FileList files={testFiles} />,
+  document.querySelector("#root")
+);
